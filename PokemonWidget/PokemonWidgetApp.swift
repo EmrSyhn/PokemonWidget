7db1efe255c54086bb42e10f6728aab5b7225580
Emir <emirseyhana.z@gmail.com>
2023-12-22 17:45:55 +0300

    Initial Commit

---
+++ PokemonWidget/PokemonWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonWidgetApp.swift
+//  PokemonWidget
+//
+//  Created by Emir Seyhan on 22.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct PokemonWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
